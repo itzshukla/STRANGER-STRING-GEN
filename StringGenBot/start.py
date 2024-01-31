768a61848beb1d4e0e14a70a37978de62714028b
STRANGER <147219905+itzshukla@users.noreply.github.com>
2024-01-31 13:53:12 +0530

    Update start.py

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -12,22 +12,18 @@ async def start(bot: Client, msg: Message):
     me2 = (await bot.get_me()).mention
     await bot.send_message(
         chat_id=msg.chat.id,
-        text=f"""𝐇𝐄𝐘💓 {msg.from_user.mention}💞💞💞💞,
-
-➳ 𝐈 𝐀𝐦 {me2}❣❣,
-𝐓𝐑𝐔𝐒𝐓𝐄𝐃 𝐒𝐓𝐑𝐈𝐍𝐆 𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐎𝐑 𝐁𝐎𝐓.
-𝐅𝐔𝐋𝐋𝐘 𝐒𝐀𝐅𝐄 & 𝐒𝐄𝐂𝐔𝐑𝐄.
-𝐍𝐎 𝐀𝐌𝐘 𝐄𝐑𝐑𝐎𝐑.
-
-𝐌𝐚𝐝𝐞 𝐁𝐲  : [𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥](tg://user?id={OWNER_ID}) !""",
+        text=f"""» ʜᴇʏ⚡️{msg.from_user.mention}  ⚡️,
+ ɪ ᴀᴍ⚡️{me2}⚡️,
+» ᴘʟᴇᴀꜱᴇ ᴄʜᴏᴏꜱᴇ ᴛʜᴇ ᴘʏᴛʜᴏɴ ʟɪʙʀᴀʀʏ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ ꜰᴏʀ.
+» ɪғ ʏᴏᴜ ɴᴇᴇᴅ ᴀɴʏ ʜᴇʟᴘ, ᴛʜᴇɴ ᴅᴍ ᴛᴏ ᴍʏ ᴏᴡɴᴇʀ: [⚡️𝗦𝗛𝗜𝗩𝗔𝗡𝗦𝗛-𝗫𝗗⚡️](tg://user?id={OWNER_ID}) !""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(text="⚡𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄 𝐒𝐓𝐑𝐈𝐍𝐆⚡", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton("❣️𝐒.𝐓x 𝐖𝐎𝐑𝐋𝐃❣️", url="https://t.me/MASTIWITHFRIENDSX"),
-                    InlineKeyboardButton("🥀 𝐒𝐇𝐈𝐕𝐀𝐍𝐒𝐇 𝐗𝐃🥀", url="https://t.me/SHIVANSH39")
+                    InlineKeyboardButton("⚡️𝗦𝗨𝗣𝗣𝗢𝗥𝗧⚡️", url="https://t.me/MASTIWITHFRIENDSX"),
+                    InlineKeyboardButton("⚡️𝗦𝗛𝗜𝗩𝗔𝗡𝗦𝗛-𝗫𝗗⚡️", url="https://t.me/SHIVANSH39")
                 ]
             ]
         ),
