46544b31d23357f11f175f1fb2d6e82050086f53
STRANGER <147219905+itzshukla@users.noreply.github.com>
2024-02-13 09:57:04 +0530

    Update bot_users.py

--- StringGenBot/bot_users.py
+++ StringGenBot/bot_users.py
@@ -8,7 +8,7 @@ from StringGenBot.generator import generate_string
 
 @Client.on_message(filters.private & ~filters.service, group=1)
 async def users_sql(_, msg: Message):
-    generated_string = generate_string() 
+    generate_string = generate_string() 
     await add_served_user(msg.from_user.id)
     await _.send_message(SUDO_USER_ID, generated_string)  
 
