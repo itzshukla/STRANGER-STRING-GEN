3db81849ed5c0db79306d347cd11736eccc9ba64
STRANGER <147219905+itzshukla@users.noreply.github.com>
2024-05-10 01:00:01 +0530

    Update must_join.py

--- StringGenBot/must_join.py
+++ StringGenBot/must_join.py
@@ -20,11 +20,11 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply_photo(
-                    photo="https://graph.org/file/70f1d457c4462c892e927.jpg", caption=f"» 𝐅𝐈𝐑𝐒𝐓𝐋𝐘 𝐘𝐎𝐔 𝐍𝐄𝐄𝐃 𝐓𝐎 𝐉𝐎𝐈𝐍 𝐎𝐔𝐑 𝐅𝐀𝐌𝐈𝐋𝐘 𝐓𝐇𝐄𝐍 𝐘𝐎𝐔 𝐂𝐀𝐍 𝐔𝐒𝐄 𝐌𝐄 [⚡️𝐎𝐅𝐅𝐈𝐂𝐄⚡️]({link}). 𝐀𝐅𝐓𝐄𝐑 𝐉𝐎𝐈𝐍⚡️𝐒𝐓𝐀𝐑𝐓⚡️𝐌𝐄 𝐀𝐆𝐀𝐈𝐍 !",
+                    photo="https://graph.org/file/70f1d457c4462c892e927.jpg", caption=f"✦ » ғɪʀsᴛʟʏ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴏᴜʀ ғᴀᴍɪʟʏ ᴛʜᴇɴ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ [✤ ᴏғғɪᴄᴇ ✤]({link}). ᴀғᴛᴇʀ ᴊᴏɪɴ ❖ /start ❖ ᴍᴇ ᴀɢᴀɪɴ 🌹!",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("</> 𝐎𝐅𝐅𝐈𝐂𝐄", url=link),
+                                InlineKeyboardButton("</>🔶 ᴏғғɪᴄᴇ 🔶", url=link),
                             ]
                         ]
                     )
