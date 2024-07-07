1ca38085fe636ad526568dbe96de85fbaa818b1a
˹sᴛʀᴀɴɢᴇʀ-ꭙᴅ⁹⁹˼  🇮🇳 <147219905+itzshukla@users.noreply.github.com>
2024-07-08 00:44:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID","27383453"))
+API_HASH = getenv("API_HASH","4c246fb0c649477cc2e79b6a178ddfaa")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("OWNER_ID","6762113050"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://SHASHANK:STRANGER@shashank.uj7lold.mongodb.net/?retryWrites=true&w=majority")
 MUST_JOIN = getenv("MUST_JOIN","SHIVANSH474")
