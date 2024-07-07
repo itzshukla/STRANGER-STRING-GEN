c453ae9522c38e59c66abd3d3d0020f7c085bb09
˹sᴛʀᴀɴɢᴇʀ-ꭙᴅ⁹⁹˼  🇮🇳 <147219905+itzshukla@users.noreply.github.com>
2024-07-08 00:24:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,34 +2,48 @@ import config
 import time
 import logging
 from pyrogram import Client, idle
-from pyromod import listen  # type: ignore
+from pyromod import listen  
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
+# Configure logging
 logging.basicConfig(
-    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    level=logging.INFO, 
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 
+logging.getLogger("pymongo").setLevel(logging.ERROR)
+
+# Initialize start time
 StartTime = time.time()
+
+# Initialize the Client
 app = Client(
-    "Anonymous",
+    "JARVIS",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN,
     in_memory=True,
-    plugins=dict(root="StringGenBot"),
+    plugins=dict(root="SESSIONGEN"),
 )
 
-
 if __name__ == "__main__":
-    print("𝐒𝐭𝐚𝐫𝐭𝐢𝐧𝐠 𝐘𝐨𝐮𝐫 𝐒𝐭𝐫𝐢𝐧𝐠 𝐁𝐨𝐭...")
+    print("𝚂𝚝𝚛𝚊𝚗𝚐𝚎𝚛 𝚂𝚎𝚜𝚜𝚒𝚘𝚗 𝙶𝚎𝚗 𝚜𝚝𝚊𝚛𝚝𝚒𝚗𝚐...")
     try:
         app.start()
-    except (ApiIdInvalid, ApiIdPublishedFlood):
-        raise Exception("Your API_ID/API_HASH is not valid.")
+    except ApiIdInvalid:
+        raise Exception("Your API_ID is not valid.")
+    except ApiIdPublishedFlood:
+        raise Exception("Your API_ID/API_HASH is flood banned.")
     except AccessTokenInvalid:
         raise Exception("Your BOT_TOKEN is not valid.")
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {e}")
+        raise
+
     uname = app.get_me().username
-    print(f"@{uname} 𝐒𝐓𝐀𝐑𝐓𝐄𝐃 𝐒𝐔𝐂𝐄𝐒𝐒𝐅𝐔𝐋𝐋𝐘. 𝐌𝐀𝐃𝐄 𝐁𝐘 @SHIVANSH39 🤗")
+    print(f"@{uname} NOW STRANGER SESSION GEN IS READY TO GEN SESSION")
+    
     idle()
+    
     app.stop()
-    print("𝗕𝗢𝗧 𝗦𝗧𝗢𝗣𝗣𝗘𝗗 𝗕𝗬 𝗕𝗬 !")
+    print("🇸 🇪 🇸 🇸 🇮 🇴 🇳  🇬 🇪 🇳 🇷 🇦 🇹 🇮 🇳 🇬  🇸 🇹 🇴 🇵 🇵 🇪 🇩...")
