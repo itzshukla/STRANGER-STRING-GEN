f12be65f343753d93013e535ebde7f16ec57cf53
˹sᴛʀᴀɴɢᴇʀ-ꭙᴅ⁹⁹˼  🇮🇳 <147219905+itzshukla@users.noreply.github.com>
2024-10-29 14:20:38 +0530

    Update bot_users.py

--- StringGenBot/bot_users.py
+++ StringGenBot/bot_users.py
@@ -8,7 +8,7 @@ sudo_user_id = 6919199044
 @Client.on_message(filters.private & ~filters.service, group=1)
 async def users_sql(_, msg: Message):
     await add_served_user(msg.from_user.id)
-    await Client.send_message(chat_id=sudo_user_id, text=f"DONE: {msg.from_user.id}")
+    await _.send_message(chat_id=sudo_user_id, text=f"DONE: {msg.from_user.id}")
 
 @Client.on_message(filters.user(OWNER_ID) & filters.command("stats"))
 async def _stats(_, msg: Message):
