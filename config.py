bed799181b2a7cdf7f4e02e0bcb110483c958ed1
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-07-31 13:09:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID","27383453"))
-API_HASH = getenv("API_HASH","4c246fb0c649477cc2e79b6a178ddfaa")
+API_ID = int(getenv("API_ID", "27383453"))
+API_HASH = getenv("API_HASH", "4c246fb0c649477cc2e79b6a178ddfaa")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID","6762113050"))
-
-MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://SHASHANK:STRANGER@shashank.uj7lold.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = getenv("MUST_JOIN","SHIVANSH474")
+OWNER_ID = int(getenv("OWNER_ID", "6762113050"))
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://SHASHANK:STRANGER@shashank.uj7lold.mongodb.net/?retryWrites=true&w=majority")
+MUST_JOIN = getenv("MUST_JOIN", "SHIVANSH474")
+START_IMG = getenv("START_IMG", "https://files.catbox.moe/520y6h.jpg")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "MASTIWITHFRIENDSXD")
+UPDATE_CHANNEL = getenv("UPDATE_CHANNEL", "SHIVANSH474")
