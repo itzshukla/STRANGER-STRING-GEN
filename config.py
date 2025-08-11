6fa225e790b3963deab4a4ec79b711c4eb28a2cb
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-08-11 19:11:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 OWNER_ID = int(getenv("OWNER_ID", "6762113050"))
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://SHASHANK:STRANGER@shashank.uj7lold.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = getenv("MUST_JOIN", "SHIVANSH474")
+MUST_JOIN = getenv("MUST_JOIN", "ITSZSHUKLA")
 START_IMG = getenv("START_IMG", "https://files.catbox.moe/520y6h.jpg")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "MASTIWITHFRIENDSXD")
 UPDATE_CHANNEL = getenv("UPDATE_CHANNEL", "SHIVANSH474")
